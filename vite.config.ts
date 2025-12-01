b9400afb7bf3e32e3b9cadbcdffba7a318239ddf
saladass99534 <166012188+saladass99534@users.noreply.github.com>
2025-12-01 15:48:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/@ffmpeg/core/dist/ffmpeg-core.wasm',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   base: './', // Important for Electron relative paths
   publicDir: 'assets',
   resolve: {
@@ -24,9 +35,11 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
     headers: {
-      // These headers are MANDATORY for ffmpeg.wasm to work (SharedArrayBuffer)
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
-    }
+    },
+  },
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
   }
 })
